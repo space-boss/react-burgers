2e18c2a64fb0903f9381026090d04d27a7f83343
Irina Herzberg <rirarum@yandex.com>
2022-01-31 21:38:52 +0100

    Fetches data from the server

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,36 @@ import AppHeader from "./components/AppHeader/AppHeader";
 import BurgerConstructor from "./components/BurgerConstructor/BurgerConstructor";
 import BurgerIngredients from "./components/BurgerIngredients/BurgerIngredients";
 
-import { data } from "./utils/data";
+const BURGER_API_URL = "https://norma.nomoreparties.space/api/ingredients";
+
+/* import { data } from "./utils/data"; */
 
 function App() {
+  const [data, setData] = React.useState([]);
+  const [loading, setIsLoading] = React.useState(false);
+  const [loadingError, setLoadingError] = React.useState("");
+
+  React.useEffect(() => {
+    const getIngredients = () => {
+      fetch(BURGER_API_URL)
+        .then((res) => {
+          setIsLoading(true);
+          return res.json();
+        })
+        .then((items) => {
+          console.log(items);
+          setIsLoading(false);
+          setData(items.data);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setLoadingError("Не удалось получить данные с сервера");
+          console.log(loadingError);
+        });
+    };
+    getIngredients();
+  }, []);
+
   return (
     <div className={styles.app}>
       <AppHeader />
