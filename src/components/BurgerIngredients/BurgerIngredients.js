1521c08343a2de881d6cab3d4769b0395f9b56b1
Irina Herzberg <rirarum@yandex.com>
2022-02-08 00:18:16 +0100

    popup opens from ingredients

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -4,67 +4,112 @@ import { dataPropType } from "../../utils/proptypes";
 import styles from "./burgerIngredients.module.css";
 import NavBar from "./NavBar/NavBar";
 import IngredientCard from "./IngredientCard/IngredientCard";
+import { IngredientDetails } from "../modal/IngredientDetails/IngredientDetails";
+import Modal from "../modal/modal";
+import ModalOverlay from "../modal/modalOverlay/modalOverlay";
 
 function BurgerIngredients(props) {
   const bunsRef = React.useRef(null);
   const sauceRef = React.useRef(null);
   const mainsRef = React.useRef(null);
 
+  const [isIngredientModalOpen, setIngredientModalOpen] = React.useState(false);
+  const [ingredientModalContent, setIngredientModalContent] = React.useState(
+    <></>
+  );
+  const [itemId, setItemId] = React.useState("");
+
+  const openIngredientModal = (evt) => {
+    const ingredientData = props.data.filter(
+      (item) => item._id === evt.currentTarget.id,
+      setItemId(evt.currentTarget.id)
+    );
+
+    setIngredientModalContent(ingredientData);
+    setIngredientModalOpen(true);
+  };
+
+  const closeIngredientModal = () => {
+    setIngredientModalContent(<></>);
+    setIngredientModalOpen(false);
+  };
+
   return (
-    <section className={styles.section}>
-      <h1 className="text text_type_main-large pt-10 pb-5">Соберите бургер</h1>
-      <NavBar bunsRef={bunsRef} sauceRef={sauceRef} mainsRef={mainsRef} />
-      <div className={styles.ingredients}>
-        <div className="mt-10" ref={bunsRef}>
-          <h2 className="text text_type_main-medium mb-6">Булки</h2>
-          <div className={`${styles.cardList} mt-6 `}>
-            {props.data.map((item) => {
-              if (item.type === "bun") {
-                return (
-                  <IngredientCard
-                    onCardClick={props.onCardClick}
-                    data={item}
-                    key={item._id}
-                  />
-                );
-              }
-            })}
+    <>
+      <section className={styles.section}>
+        <h1 className="text text_type_main-large pt-10 pb-5">
+          Соберите бургер
+        </h1>
+        <NavBar bunsRef={bunsRef} sauceRef={sauceRef} mainsRef={mainsRef} />
+        <div className={styles.ingredients}>
+          <div className="mt-10" ref={bunsRef}>
+            <h2 className="text text_type_main-medium mb-6">Булки</h2>
+            <div className={`${styles.cardList} mt-6 `}>
+              {props.data.map((item) => {
+                if (item.type === "bun") {
+                  return (
+                    <IngredientCard
+                      onClick={openIngredientModal}
+                      data={item}
+                      key={item._id}
+                      id={item._id}
+                    />
+                  );
+                }
+              })}
+            </div>
+          </div>
+          <div className="mt-10" ref={sauceRef}>
+            <h2 className="text text_type_main-medium mb-6">Соусы</h2>
+            <div className={`${styles.cardList} mt-6 `}>
+              {props.data.map((item) => {
+                if (item.type === "sauce") {
+                  return (
+                    <IngredientCard
+                      onClick={openIngredientModal}
+                      data={item}
+                      key={item._id}
+                      id={item._id}
+                    />
+                  );
+                }
+              })}
+            </div>
+          </div>
+          <div className="mt-10" ref={mainsRef}>
+            <h2 className="text text_type_main-medium mb-6">Начинки</h2>
+            <div className={`${styles.cardList} mt-6 `}>
+              {props.data.map((item) => {
+                if (item.type === "main") {
+                  return (
+                    <IngredientCard
+                      onClick={openIngredientModal}
+                      data={item}
+                      key={item._id}
+                      id={item._id}
+                    />
+                  );
+                }
+              })}
+            </div>
           </div>
         </div>
-        <div className="mt-10" ref={sauceRef}>
-          <h2 className="text text_type_main-medium mb-6">Соусы</h2>
-          <div className={`${styles.cardList} mt-6 `}>
-            {props.data.map((item) => {
-              if (item.type === "sauce") {
-                return (
-                  <IngredientCard
-                    onCardClick={props.onCardClick}
-                    data={item}
-                    key={item._id}
-                  />
-                );
-              }
-            })}
-          </div>
-        </div>
-        <div className="mt-10" ref={mainsRef}>
-          <h2 className="text text_type_main-medium mb-6">Начинки</h2>
-          <div className={`${styles.cardList} mt-6 `}>
-            {props.data.map((item) => {
-              if (item.type === "main") {
-                return (
-                  <IngredientCard
-                    onClick={props.onCardClick}
-                    data={item}
-                    key={item._id}
-                  />
-                );
-              }
-            })}
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+      <Modal
+        onClose={closeIngredientModal}
+        isOpen={isIngredientModalOpen}
+        title="Детали ингредиента"
+      >
+        <IngredientDetails
+          ingredientContent={ingredientModalContent}
+          itemId={itemId}
+        />
+      </Modal>
+      <ModalOverlay
+        onClick={closeIngredientModal}
+        isOpen={isIngredientModalOpen}
+      />
+    </>
   );
 }
 
--- src/components/BurgerIngredients/IngredientCard/IngredientCard.js
+++ src/components/BurgerIngredients/IngredientCard/IngredientCard.js
@@ -16,18 +16,20 @@ function IngredientCard(props) {
 
   return (
     <div className={`${styles.card} ml-4 mr-2 mt-6 mb-8`}>
-      <img src={props.data.image} alt={props.data.name} />
       <button className={styles.button} onClick={handleCounterClick}>
         <Counter size="default" count={counterValue} />
       </button>
+      <button className={styles.button} onClick={props.onClick} id={props.id}>
+        <img src={props.data.image} alt={props.data.name} />
 
-      <div className={styles.price}>
-        <h3 className="text text_type_main-medium m-2">{props.data.price}</h3>
-        <div className={styles.icon}>
-          <CurrencyIcon type="primary" />
+        <div className={styles.price}>
+          <h3 className="text text_type_main-medium m-2">{props.data.price}</h3>
+          <div className={styles.icon}>
+            <CurrencyIcon type="primary" />
+          </div>
         </div>
-      </div>
-      <p className="text text_type_main-default">{props.data.name}</p>
+        <p className="text text_type_main-default">{props.data.name}</p>
+      </button>
     </div>
   );
 }
